7a9779cb89756c1b5107d95d3e418f6b0a37c2ee
weishi-qian <2358735296@qq.com>
2026-07-25 16:32:40 +0800

    上传全部项目代码

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const multer = require('multer');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 确保上传目录存在
 const fs = require('fs');
